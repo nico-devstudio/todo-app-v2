bf618023c6f32050ee6c1c839250e00bae37cdbb
Nico Angelo  Garilva <nicogarilva@gmail.com>
2026-08-05 04:50:46 +0800

    adding edit features

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const form = document.getElementById("todo-form");
-const input = document.getElementById("todo-input")
+const input = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+const filterButtons = document.querySelectorAll("[data-filter]")
 
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
@@ -9,10 +11,10 @@ let todos = JSON.parse(localStorage.getItem("todos")) || [];
 // STATE
 // ======================
 
-let text = input.value.trim()
-
 form.addEventListener("submit", function (e) {
     e.preventDefault()
+
+    const text = input.value.trim()
 
     if (text === "") return
 
@@ -22,11 +24,101 @@ form.addEventListener("submit", function (e) {
         completed: false
     };
 
+    todos.push(todo)
+
     saveTodos();
     renderTodos();
     input.value = "";
 })
 
 // ======================
-//
+// Local Storage
 // ======================
+
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+// ======================
+// Filter State
+// ======================
+
+let currentFilter = "all";
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        currentFilter = button.dataset.filter;
+        renderTodos();
+    })
+})
+
+
+// ======================
+// Render Todo
+// ======================
+
+function renderTodos() {
+    todoList.innerHTML = "";
+
+    let filteredTodos = todos.filter(todo => {
+        if (currentFilter === "completed") {
+            return todo.completed === true
+        } else if (currentFilter === "pending") {
+            return todo.completed === false
+        }
+
+        return true;
+    })
+
+    filteredTodos.forEach(todo => {
+
+
+        const li = document.createElement("li");
+        li.className = "todo-item"
+
+        li.innerHTML = `<span class = "${todo.completed ? "completed" : ""}">
+                    ${todo.text}  </span> 
+                    <div>
+
+                   <button>Delete </button>`;
+
+        li.querySelector("span").addEventListener("click", function () {
+            toggleComplete(todo.id)
+        })
+
+        li.querySelector("button").addEventListener("click", function () {
+            deleteTodo(todo.id)
+        })
+
+        todoList.appendChild(li)
+    });
+}
+
+renderTodos()
+
+// ======================
+// Toggle complete
+// ======================
+
+function toggleComplete(id) {
+    todos = todos.map(todo => {
+        if (todo.id === id) {
+            todo.completed = !todo.completed
+        }
+        return todo
+    })
+    saveTodos()
+    renderTodos()
+}
+
+// ======================
+// Delete Todo
+// ======================
+
+function deleteTodo(id) {
+    todos = todos.filter(todo => {
+        return todo.id !== id
+    })
+    saveTodos()
+    renderTodos()
+}
