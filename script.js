b009d7561e85a0437a01be68bb3a5804270b2b3d
Nico Angelo  Garilva <nicogarilva@gmail.com>
2026-08-06 04:26:55 +0800

    added currentFilter

--- script.js
+++ script.js
@@ -3,41 +3,39 @@ const input = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
 const filterButtons = document.querySelectorAll("[data-filter]")
 
-
-let todos = JSON.parse(localStorage.getItem("todos")) || [];
-
-
 // ======================
 // STATE
 // ======================
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault()
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-    const text = input.value.trim()
 
-    if (text === "") return
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let text = input.value.trim();
+
+    if (text === "") return;
 
     const todo = {
         id: Date.now(),
         text,
         completed: false,
-        editing: false,
+        editing: false
     };
 
-    todos.push(todo)
+    todos.push(todo);
 
-    saveTodos();
-    renderTodos();
+    saveTodos()
+    renderTodos()
     input.value = "";
 })
 
 // ======================
-// Local Storage
+// LOCAL STORAGE
 // ======================
 
 function saveTodos() {
-    localStorage.setItem("todos", JSON.stringify(todos))
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 // ======================
@@ -48,138 +46,34 @@ let currentFilter = "all";
 
 filterButtons.forEach(button => {
     button.addEventListener("click", () => {
-        currentFilter = button.dataset.filter;
-        renderTodos();
+        currentFilter = button.dataset.filter
     })
 })
 
 
 // ======================
-// Render Todo
+// Render
 // ======================
 
 function renderTodos() {
     todoList.innerHTML = "";
 
     let filteredTodos = todos.filter(todo => {
-        if (currentFilter === "completed") {
-            return todo.completed === true
-        } else if (currentFilter === "pending") {
-            return todo.completed === false
+        if (currentFilter === "pending") {
+            return !todo.completed;
+        } else if (currentFilter === "completed") {
+            return todo.completed;
         }
-
-        return true;
+        return true
     })
 
-    filteredTodos.forEach(todo => {
+
+    filteredTodos.forEach(() => {
 
 
         const li = document.createElement("li");
-        li.className = "todo-item";
-        if (todo.editing === false) {
-            li.innerHTML = `<span class = "${todo.completed ? "completed" : ""}">
-                    ${todo.text}  </span> 
-                    <div>
-                    <button class="edit-btn">Edit</button>
-                   <button class="delete-btn">Delete</button>
-                   </div>`;
-            li.querySelector("span").addEventListener("click", function () {
-                toggleComplete(todo.id)
-            })
-            li.querySelector(".edit-btn").addEventListener("click", function () {
-                startEdit(todo.id)
-            })
-            li.querySelector(".delete-btn").addEventListener("click", function () {
-                deleteTodo(todo.id)
-            })
-        } else {
-            li.innerHTML = `<input value="${todo.text}">
-                    <button class="save-btn">Save</button>
-                  `;
-
-            let editInput = li.querySelector("input")
-            li.querySelector(".save-btn").addEventListener("click", function () {
-                saveEdit(todo.id, editInput.value);
-            })
-
-            editInput.addEventListener("keydown", (e) => {
-                if (e.key === "Enter") {
-                    saveEdit(todo.id, editInput.value);
-                }
-            })
-        }
-
-        todoList.appendChild(li)
-    });
-}
-
-renderTodos()
-
-// ======================
-// Toggle complete
-// ======================
-
-function toggleComplete(id) {
-    todos = todos.map(todo => {
-        if (todo.id === id) {
-            todo.completed = !todo.completed
-        }
-        return todo
+        li.className("todo-item");
+        li.innerHTML = ``
     })
-    saveTodos()
-    renderTodos()
-}
-
-// ======================
-// Delete Todo
-// ======================
-
-function deleteTodo(id) {
-    todos = todos.filter(todo => {
-        return todo.id !== id
-    })
-    saveTodos()
-    renderTodos()
-}
-
-// ======================
-// Edit Todo
-// ======================
-
-
-function startEdit(id) {
-    todos = todos.map(todo => {
-        if (todo.id === id) {
-            todo.editing = true;
-        }
-
-        return todo;
-    })
-
-    saveTodos();
-    renderTodos();
-}
-
-// ======================
-// Save Edit
-// ======================
-
-function saveEdit(id, newText) {
-
-    newText = newText.trim();
-
-    if (newText === "") return;
-
-    todos = todos.map(todo => {
-        if (todo.id === id) {
-
-            todo.text = newText;
-            todo.editing = false;
-        }
-        return todo;
-
-    })
-    saveTodos();
-    renderTodos();
 
 }
